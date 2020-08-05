a0a158ce39acf8a43c6a27e8276378611eaa7ec4
FreshyMichael <50490309+FreshyMichael@users.noreply.github.com>
2020-08-05 13:33:57 -0400

    Update update-this-plugin-name.php

--- update-this-plugin-name.php
+++ update-this-plugin-name.php
@@ -1,7 +1,7 @@
 <?php
 /**
-* Plugin Name: Add PluginName
-* Plugin URI: https://github.com/FreshyMichael/Plugin-Starter
+* Plugin Name: Example PostTypeName Custom Post Type
+* Plugin URI: https://github.com/FreshyMichael/CustomPostType-Template
 * Description: Add a Description
 * Version: 1.0.0
 * Author: FreshySites
@@ -15,7 +15,45 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /* PluginName Start */
 //______________________________________________________________________________
+function example_register_post_type() {
 
+	$labels = array(
+		'name' => __( 'PostTypeName', 'example' ),
+		'singular_name' => __( 'PostTypeName', 'example' ),
+		'add_new' => __( 'New PostTypeName', 'example' ),
+		'add_new_item' => __( 'Add New PostTypeName', 'example' ),
+		'edit_item' => __( 'Edit PostTypeName', 'example' ),
+		'new_item' => __( 'New PostTypeName', 'example' ),
+		'view_item' => __( 'View PostTypeNames', 'example' ),
+		'search_items' => __( 'Search PostTypeNames', 'example' ),
+		'not_found' =>  __( 'No PostTypeNames Found', 'example' ),
+		'not_found_in_trash' => __( 'No PostTypeNames found in Trash', 'example' ),
+	);
+
+	$args = array(
+ 		'labels' => $labels,
+ 		'has_archive' => true,
+ 		'public' => true,
+ 		'hierarchical' => false,
+ 		'supports' => array(
+  		'title',
+  		'editor',
+  		'excerpt',
+  		'custom-fields',
+  		'thumbnail',
+  		'page-attributes'
+ 		),
+		'menu_icon' => 'dashicons-media-text',
+ 		'taxonomies' => 'category',
+ 		'rewrite'   => array( 'slug' => 'lca' ),
+ 		'show_in_rest' => true,
+	);
+
+	register_post_type( 'example_PostTypeName', $args );
+
+}
+
+add_action( 'init', 'example_register_post_type' );
 
 //______________________________________________________________________________
 // All About Updates
@@ -24,10 +62,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 // ***IMPORTANT*** Update this path to New Github Repository Master Branch Path
-	'https://github.com/FreshyMichael/Plugin-Starter',
+	'https://github.com/FreshyMichael/CustomPostType-Template',
 	__FILE__,
 // ***IMPORTANT*** Update this to New Repository Master Branch Path
-	'Plugin-Starter'
+	'CustomPostType-Template'
 );
 //Enable Releases
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
